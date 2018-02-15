d12c788b12ad6fc841946845cc21e20116fec703
windyakin <windyakin@gmail.com>
2018-02-15 13:48:57 +0900

    :recycle: サイトにアクセスするごとにページを作り直す

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ logger.level = 'debug';
   const puppeteerParam = (os.platform() === 'linux' ? ['--no-sandbox', '--disable-setuid-sandbox'] : []);
 
   const browser = await Puppeteer.launch({ args: puppeteerParam });
-  const page = await browser.newPage();
-  page.setViewport({ width: 1280, height: 720 });
 
   await new Promise(resolve => resolve(
     websites,
     Array.from(Array(websites.length).keys()),
   )).each(async (website, index) => {
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 720 });
     const fileIndex = (index + 1).toString().padStart(2, '0');
     const filePath = `${folderName}/${fileIndex}_${website.name}.png`;
     logger.info(`Get ${website.name} (${website.url}) ...`);
@@ -39,6 +39,7 @@ logger.level = 'debug';
       }
       return;
     }
+    await page.close();
     logger.info(`Saved! ${filePath}`);
   });
 
